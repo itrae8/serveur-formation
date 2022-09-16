7137753ebe5b2d55f75a433e9614f81432ede49b
itrae8 <thibaut_detrois@hotmail.fr>
2022-09-16 18:34:54 +0200

    Ajout patch acteur

--- server-formation.js
+++ server-formation.js
@@ -5,10 +5,10 @@ module.exports = (req, res, next) => {
     // Retour spécifique pour coller au comportement de l'EX
     if (req.method === 'PATCH') {
 
-        if (req.url === '/film') {
+        if (req.url === '/acteur') {
             const body = req.body;
-            if (body.id === 'fight-club') {
-                return res.status(400).send({ message: "Erreur lors de la modification des informations du film Fight club" });
+            if (body.id === 'brad-pitt') {
+                return res.status(400).send({ message: "Erreur lors de la modification des informations de l'acteur Brad Pitt" });
             } else {
                 return res.status(201).send(body);
             }
